da150da711714914ca496955ad680546208033c9
Halen Solomon <halensolomon@gwu.edu>
2024-03-25 20:32:50 -0400

    Implemented ability to load images and kernels from .tiff/.tif files.

--- CPP_RL/cuda_rl.cu
+++ CPP_RL/cuda_rl.cu
@@ -2,64 +2,69 @@
 #include <cuda_runtime.h>
 #include <cudnn.h>
 #include <torch/torch.h>
-#include <cuda_rl.cuh> 
+#include "cuda_rl.cuh"
+#include "fftconv.cu"
+#include <opencv2/imgcodecs.hpp>
+
+namespace fs = std:filesystem;
 
 /// One image, multiple kernels: GPU implementation of the 2D convolution operation
 /// Zero-copy for image and kernel data
 /// Pinned memory for result data, since there will be multiple iterations
 
-__global__ void zeroCopyFFTConv(float *image, float *kernel, int *imageSize)
+void fileSearch(const std::string &path, const std::string &ext std::vector<std::string> &filepaths)
 {
-    // Get the thread index
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    int idy = blockIdx.y * blockDim.y + threadIdx.y;
-
-    // Get the image size
-    int imgx = imageSize[0];
-    int imgy = imageSize[1];
-
-    // Check if the thread is within the bounds of the image
-    if (idx < imgx && idy < imgy)
+    for (const auto &p : fs::recursive_directory_iterator(path))
     {
-        
+        if (p.path().extension() == ext)
+            filePaths.push_back(p.path().string());
+    }
+}
+
+std::vector<float>* readImage(const std::string &path)
+{
+    /// Read the image and store it in a vector
+    cv::Mat MatImage = cv::imread(path, cv::IMREAD_UNCHANGED);
+    std::vector<float>* ImagePtr = new std::vector<float>;
+
+    if (MatImage.empty())
+    {
+        std::cout << "Could not read the image: " << path << std::endl;
+        exit(0);
     }
 
+    if (MatImage.isContinuous())
+    {
+        ImagePtr->assign(reinterpret_cast<float*>(MatImage.data), reinterpret_cast<float*>(MatImage.data) + MatImage.total() * MatImage.channels());
+        MatImage.release();
+    }
+    else
+    {
+        std::cout << "Image is not continuous" << std::endl;
+        exit(1);
+    }
 
-
+    return ImagePtr;
 }
 
+void flipAdd2One(std::vector<std::vector<float>>& originalArray, std::vector<std::vector<float>>& flippedArray) {
+    /// Flip the array and add the original and flipped arrays to one
+    int numRows = originalArray.size();
+    int numCols = originalArray[0].size();
 
-int main()
-{
-    // Intialize the image data
-    float *imageData;
-    cudaMallocHost(&imageData, IMG_SIZE * IMG_SIZE * sizeof(float));
+    std::vector<std::vector<float>> flippedArray(numRows, std::vector<float>(numCols));
 
-    // Initialize kernels
-
-    
-
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numCols; j++) {
+            float sum = originalArray[i][j] + flippedArray[i][j];
+            originalArray[i][j] /= sum;
+            flippedArray[i][j] /= sum;
+            flippedArray[i][numCols - j - 1] = array[i][j];
+        }
+    }
 }
 
-
-
-
-// PyTorch C++ frontend to load tensors from .pt files
-std::vector<torch::Tensor> tensor_vec;
-torch::load(tensor_vec, "path/to/tensor.pt");
-
-
-
-
-
-__global__ void fft2D{
-
-
-
-
-}
-
-__global__ void rlAlg(int *imgdata, int *kern, int *result, int *imgsize, int *kernsize, int *numkern)
+__global__ void rlAlg(int *imgdata, int *kern, int *result, int *imgsize, int *kernsize, int *numkern, int *iterations)
 {
     // Get the thread index
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -81,32 +86,33 @@ __global__ void rlAlg(int *imgdata, int *kern, int *result, int *imgsize, int *k
     int resy = imgy - kerny + 1;
     int resz = imgz - kernz + 1;
 
-    // Check if the thread is within the bounds of the result
-    if (idx < resx && idy < resy && idz < resz)
-    {
-        // Initialize the result
-        result[idz * resx * resy + idy * resx + idx] = 0;
+    // Intialize result data to be a cylinder of ones
+    results = 1;
 
-        // Loop over the kernel
-        for (int kx = 0; kx < kernx; kx++)
-        {
-            for (int ky = 0; ky < kerny; ky++)
-            {
-                for (int kz = 0; kz < kernz; kz++)
-                {
-                    // Add the product of the image and kernel to the result
-                    result[idz * resx * resy + idy * resx + idx] += imgdata[(idz + kz) * imgx * imgy + (idy + ky) * imgx + (idx + kx)] * kern[kz * kernx * kerny + ky * kernx + kx];
-                }
-            }
-        }
+    int centerx = resx / 2;
+    int centery = resy / 2;
+
+    for (int kz = 0; kz <= interations; kz++)
+    {
+        
+
+
+
+
+        
     }
+    
 }
-__shared__ float imgdata[IMG_SIZE];
+
 
 int main(int argc, char** argv)
 {
     int numGPUs;
     cudaGetDeviceCount(&numGPUs);
+    std::cout << "Number of GPUs: " << numGPUs << std::endl;
+
+    float *imgData = new float[imgX * imgY];
+    float *kernData = new float[imgX * imgY * kernZ];
 
     // Use all available GPUs
     for (int i = 0; i < numGPUs; i++)
@@ -115,7 +121,96 @@ int main(int argc, char** argv)
         cudaDeviceEnablePeerAccess(i, 0);
     }
 
-    // Initialize the image data
+    // Search for all the images in the directory
+    std::vector<std::string> imgPaths;
+    std::vector<std::string> kernPaths;
+    std::string imgPath = "C:/some/path/to/images/";
+    std::string kernPath = "C:/some/path/to/kernels/";
+
+    fileSearch(imgPath, ".tif", imgPaths);
+    int numImages = imgPaths.size();
+
+    fileSearch(kernPath, ".tif", kernPaths);
+    int numKernels = kernPaths.size();
+    
+    std::vector<std::vector<float>*> kernPtrStore;
+
+    /// Read all the kernels
+    for (int i = 0; i < numKernels; i++)
+    {
+        /// Read kernel
+        std::vector<float>* kernPtr = readImage(kernPaths[i]);
+        if (kernPtr != nullptr)
+        {
+            std::cout << "Kernel read successfullly" << std::end1;
+            float* kernPinnedMem; // Pinned memory for kernel data
+            size_t kernByteSize = kernPtr->size() * sizeof(float); // Size of the kernel in bytes
+            cudaError_t error = cudaMallocHost((void**)&kernPinnedMem, kernByteSize); // Allocate pinned memory for kernel data
+            kernPtr.push_back(kernPinnedMem); // Store the kernel data in a vector
+        }
+
+        if (error != cudaSuccess) 
+        {
+            std::cerr << "Failed to allocate pinned memory for kernel data" << std::endl;
+        } 
+        else 
+        {
+            cuMemcpy(kernPinnedMem, kernPtr->data(), kernByteSize, cudaMemcpyHostToDevice);
+            kernPtrStore.push_back(kernPinnedMem); // Store the allocated pinned memory address
+        }
+    }
+
+    for (int i = 0; i < numKernels, i++)
+    {
+        std::vector<float>* backKernPtr = kernPtrStore[i];
+
+        float* backKernPinnedMem = backKernPtr->data();
+        size_t backKernByteSize = backKernPtr->size() * sizeof(float);
+
+        cudaError_t error = cudaMemcpy(backKernPinnedMem, backKernPtr->data(), backKernByteSize, cudaMemcpyHostToDevice);
+
+    }
+
+
+
+
+    /// Read images sequentially
+    for (i = 0, i < Images, i++)
+    {
+        std::vector<float>* imgPtr = readImage(imgPaths[i]);
+        if (imgPtr != nullptr)
+        {
+            std::cout << "Image read successfully" << std::endl;
+            float* imgPinnedMem; // Pinned memory for image data
+            size_t imgByteSize = imgPtr->size() * sizeof(float); // Size of the image in bytes
+            cudaError_t error = cudaMallocHost((void**)&imgPinnedMem, imgByteSize); // Allocate pinned memory for image data
+
+            // Copy data from the vector to pinned memory
+            std::memcpy(imgPinnedMem, imgPtr->data(), imgByteSize); // Copy the image data to pinned memory
+
+            if (error != cudaSuccess) 
+            {
+                std::cerr << "Failed to allocate pinned memory for image data" << std::endl;
+                delete ImagePtr; // Release ImagePtr if allocation fails
+            }
+
+        std::cout << "Image size is: " << imgPtr->size() << std::endl;
+        std::cout << "Kernel size is: " << kernPtr->size() << std::endl;
+
+        if (imgPtr->size() != kernPtr->size())
+        {
+            std::cerr << "Image and kernel sizes do not match" << std::endl;
+            exit(2);
+        }
+
+        //
+
+        }
+    } 
+
+
+
+    
 
     // Initialize the kernel
 
